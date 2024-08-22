e9001cc91f24d45406a20502cf9aa4364831f71f
Thurein <thureintun@acedatasystems.com>
2024-08-22 11:04:10 +0630

    add custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'Matemasie-Regular': require('./assets/fonts/Matemasie-Regular.ttf'),
+    'RobotoMono-Regular': require('./assets/fonts/RobotoMono-Regular.ttf'),
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'RobotoMono-Regular' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
